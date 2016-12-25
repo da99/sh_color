b50a149e14de43a1d7d1036ae02f98435363e4c8
da99 <i-hate-spam-1234567@mailinator.com>
2016-12-25 02:56:09 -0600

    Fixed: typos.

--- bin/public/RED/specs.sh
+++ bin/public/RED/specs.sh
@@ -5,10 +5,10 @@ specs () {
   local +x Green='\e[0;32m'
 
   should-match "$(echo -e this is ${BRed}red${Reset})" \
-    "$(bash_setup RED "this is {{red}}" 2>&1)"
+    "$(sh_color RED "this is {{red}}" 2>&1)"
   should-match "$(echo -e this is ${BRed}red: {red}${Reset})" \
-    "$(bash_setup RED "this is {{red: {red}}}" 2>&1)"
+    "$(sh_color RED "this is {{red: {red}}}" 2>&1)"
   should-match "$(echo -e this is ${BRed}red: {red}${Reset} and ${Green}green${Reset})" \
-    "$(bash_setup RED "this is {{red: {red}}} and GREEN{{green}}" 2>&1)"
+    "$(sh_color RED "this is {{red: {red}}} and GREEN{{green}}" 2>&1)"
 }
 specs
